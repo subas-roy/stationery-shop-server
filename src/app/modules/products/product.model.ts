dbf754b3e035ad3858f1a2aed6d0f9290f11fbdb
subas-roy <subasroy46@gmail.com>
2024-11-30 18:20:55 +0600

    Create productSchema and Product model

---
+++ src/app/modules/products/product.model.ts
@@ -0,0 +1,45 @@
+// Import required modules from Mongoose
+import { model, Schema } from 'mongoose';
+
+/**
+ * Define the schema for the `Product` model
+ * - Represents the structure of product data stored in the database
+ */
+const productSchema = new Schema({
+  name: {
+    type: String, // Product name as a string
+    required: true, // Field is mandatory
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: [String], // Array of strings representing product categories
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number, // Field is mandatory
+  },
+  inStock: {
+    type: Boolean, // Indicates if the product is in stock
+    default: true, // Defaults to `true` if not provided
+  },
+});
+
+/**
+ * Create the `Product` model
+ * - The model will be used to interact with the `products` collection in the database
+ */
+const Product = model('Product', productSchema);
+
+// Export the model for use in other parts of the application
+export default Product;
